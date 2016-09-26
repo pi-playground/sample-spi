98e335922bab8d7b8e69805e805da8852bb32d63
YongliuLi <ylli@thoughtworks.com>
2016-09-26 23:23:05 +0800

    [#0] add latch data

--- test_0on_1toggle.py
+++ test_0on_1toggle.py
@@ -1,13 +1,26 @@
 import spidev
 import time
+from  gpiozero import DigitalOutputDevice
+switch =  DigitalOutputDevice(12, initial_value=0)
+switch.off()
+
+def latchData:
+	switch.on()
+	time.sleep(0.001)
+	switch.off()
+
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.writebytes([0x00,0x00])
+latchData()
 time.sleep(3)
 spi.writebytes([0xff,0x00])
+latchData()
 time.sleep(3)
 spi.writebytes([0x00,0x00])
+latchData()
 time.sleep(3)
 spi.writebytes([0xff,0x00])
+latchData()
 time.sleep(3)
 spi.close()
