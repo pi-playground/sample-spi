87a2ee51049aaab9eb320efec7aea6606dfeb92d
YongliuLi <ylli@thoughtworks.com>
2016-09-26 23:01:56 +0800

    [#0] add 595 sample

---
+++ test_0on_1toggle.py
@@ -0,0 +1,12 @@
+import spidev
+import time
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.writebytes([0x00,0x00])
+time.sleep(3)
+spi.writebytes([0xff,0x00])
+time.sleep(3)
+spi.writebytes([0x00,0x00])
+time.sleep(3)
+spi.writebytes([0xff,0x00])
+time.sleep(3)
